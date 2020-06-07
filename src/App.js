17a0446c78c6921a8a77c42973aec0d1e8368e5e
Brian-McHugh <125cedarstreet@gmail.com>
2020-06-07 18:46:34 -0500

    Add styling to ContactList

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ContactList from './components/ContactList';
+import User from './components/User';
+
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "@karen_isgrigg",
+    "avatarURL": "../images/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "@richardkalehoff",
+    "avatarURL": "../images/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "@tylermcginnis",
+    "avatarURL": "../images/tyler.jpg"
+  }
+];
 
 class App extends React.Component {
   render() {
     
     return (
       <div className="App">
-        <ContactList contacts={[
-          {name: 'Tyler'},
-          {name: 'Kim'},
-          {name: 'Richard'}
-        ]}/>
-        <ContactList contacts={[
-          {name: 'Jim'},
-          {name: 'Allison'},
-          {name: 'Natalie'}
-        ]}/>
+        <ContactList contacts={contacts}/>
       </div>
     );
   }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,13 +2,23 @@ import React from 'react';
 
 class ContactList extends React.Component {
   render() {
-    const people = this.props.contacts;
-    
+
     return (
-      <ol>
-        {people.map(person => (
-          <li key={person.name}>
-            {person.name}
+      <ol className="contact-list">
+        {this.props.contacts.map((contact) => (
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar" style={{
+              backgroundImage: `url(${contact.avatarURL})`
+              }}
+            ></div>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button className="contact-remove">
+              Remove
+            </button>
           </li>
         ))}
       </ol>
